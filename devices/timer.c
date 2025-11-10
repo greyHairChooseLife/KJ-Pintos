693a39d37b44f6c619536599ba1d26862af48d71
sangyeon kim <greyhairchooselife@gmail.com>
2025-11-10 19:28:12 +0900

    Revert "etc: revealing busy waiting"
    This reverts commit 28529fc9fba2af8e529fbdec91b7744f5a283f51.

--- devices/timer.c
+++ devices/timer.c
@@ -86,14 +86,7 @@ void timer_sleep(int64_t ticks) {
     int64_t start = timer_ticks();
 
     ASSERT(intr_get_level() == INTR_ON);
-    int count = 0;
-
-    while (timer_elapsed(start) < ticks)
-    {
-        printf("%s: %dth call: I'm not ready!\n", thread_current()->name,
-               ++count);
-        thread_yield();
-    }
+    while (timer_elapsed(start) < ticks) thread_yield();
 }
 
 /* Suspends execution for approximately MS milliseconds. */
